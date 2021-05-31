b762ff05749113ac1838914588d3968fbb47f2fa
Rohith <marganirohith2000@gmail.com>
2021-06-01 01:14:47 +0530

    second

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const box1 = document.querySelector(".one");
+const body1 = document.querySelector(".body-one");
+
+const menu = document.getElementById("mobile-cta");
+const modal = document.querySelector(".modal-full");
+const titlepage = document.querySelector(".mid-col");
+
+menu.addEventListener("click", function () {
+  console.log("clicked");
+});
